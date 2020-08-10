acb792d7e3f1f931f9655490687cb63863620e9c
Matheus Santos <matheus.san99@gmail.com>
2020-08-10 17:26:42 -0300

    Minor changes to the Cast tab

--- mobile/src/pages/Overview/Tabs/Elenco/index.tsx
+++ mobile/src/pages/Overview/Tabs/Elenco/index.tsx
@@ -37,9 +37,23 @@ const Elenco = ({ params, isTooWhite }: { params: Params, isTooWhite: boolean })
 
     function renderItem({ item, index }: { item: Cast, index: number }) {
         if (index == 0 && params.itemParam.type == 'movie') {
+            let profilePath = ''
+            let directorProfile = ''
+
+            if(item.profile_path == null) {
+                profilePath = 'https://m.media-amazon.com/images/G/01/IMDbPro/images/default_name._V142442227_UY289_CR46,0,196,289_.png'
+            } else {
+                profilePath = `https://image.tmdb.org/t/p/w200/${item.profile_path}`
+            }
+
+            if(director?.profile_path == null){
+                directorProfile = 'https://m.media-amazon.com/images/G/01/IMDbPro/images/default_name._V142442227_UY289_CR46,0,196,289_.png'
+            } else {
+                directorProfile = `https://image.tmdb.org/t/p/w200/${director?.profile_path}`
+            }
             return (
                 <View>
-                    <Text style={[styles.rowTitle, { color: cardColor }]}>Director</Text>
+                    <Text style={[styles.rowTitle, { color: cardColor }]}>Diretor</Text>
                     <TouchableOpacity style={[styles.card, { backgroundColor: cardColor }]} activeOpacity={0.7}>
                         <View>
                             <View style={styles.textBlock}>
@@ -49,7 +63,7 @@ const Elenco = ({ params, isTooWhite }: { params: Params, isTooWhite: boolean })
 
                         </View>
                         <Image
-                            source={{ uri: `https://image.tmdb.org/t/p/w200/${director?.profile_path}` }}
+                            source={{ uri: directorProfile }}
                             style={styles.profileImg}
                         />
                     </TouchableOpacity>
@@ -68,32 +82,41 @@ const Elenco = ({ params, isTooWhite }: { params: Params, isTooWhite: boolean })
                             </View>
                         </View>
                         <Image
-                            source={{ uri: `https://image.tmdb.org/t/p/w200/${item.profile_path}` }}
+                            source={{ uri: profilePath }}
                             style={styles.profileImg}
                         />
                     </TouchableOpacity>
                 </View>
             )
-        }
-        return (
-            <TouchableOpacity style={[styles.card, { backgroundColor: cardColor }]} activeOpacity={0.7}>
-                <View>
-                    <View style={styles.textBlock}>
-                        <Text style={[styles.title, { color: textColor }]}>Nome: {' '}</Text>
-                        <Text style={[styles.text, { color: textColor }]}>{item.name}</Text>
-                    </View>
+        } else {
+            let profilePath = ''
 
-                    <View style={styles.textBlock}>
-                        <Text style={[styles.title, { color: textColor }]}>Personagem: {' '}</Text>
-                        <Text style={[styles.text, { color: textColor }]}>{item.character}</Text>
+            if(item.profile_path == null) {
+                profilePath = 'https://m.media-amazon.com/images/G/01/IMDbPro/images/default_name._V142442227_UY289_CR46,0,196,289_.png'
+            } else {
+                profilePath = `https://image.tmdb.org/t/p/w200/${item.profile_path}`
+            }
+
+            return (
+                <TouchableOpacity style={[styles.card, { backgroundColor: cardColor }]} activeOpacity={0.7}>
+                    <View>
+                        <View style={styles.textBlock}>
+                            <Text style={[styles.title, { color: textColor }]}>Nome: {' '}</Text>
+                            <Text style={[styles.text, { color: textColor }]}>{item.name}</Text>
+                        </View>
+    
+                        <View style={styles.textBlock}>
+                            <Text style={[styles.title, { color: textColor }]}>Personagem: {' '}</Text>
+                            <Text style={[styles.text, { color: textColor }]}>{item.character}</Text>
+                        </View>
                     </View>
-                </View>
-                <Image
-                    source={{ uri: `https://image.tmdb.org/t/p/w200/${item.profile_path}` }}
-                    style={styles.profileImg}
-                />
-            </TouchableOpacity>
-        )
+                    <Image
+                        source={{ uri: profilePath }}
+                        style={styles.profileImg}
+                    />
+                </TouchableOpacity>
+            )
+        }
     }
 
     return (
